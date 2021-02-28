3f7058347cc4c9139838c9da7b7092a0ebd8738e
ElenaIf <ivankina.es@gmail.com>
2021-02-28 16:50:57 +0200

    fixed mobile version, added burger menu, added back to top button

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,14 +1,35 @@
 let header = document.querySelector("header");
+let buttonTop = document.querySelector("#topButton");
+let buttonMobile = document.querySelector("#mobileButton");
+let navigation = document.querySelector("nav");
+let links = document.querySelectorAll("nav ul li a");
 
 const changeHeader = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     header.className = "newHeader";
+    buttonTop.style.display = "block";
   } else {
     header.className = "";
+    buttonTop.style.display = "none";
   }
 };
 
+const mobMenu = () => {
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", mobMenu);
+  }
+  navigation.classList.toggle("responsive");
+  header.classList.toggle("bgWhite");
+};
+
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
 window.addEventListener("scroll", changeHeader);
+buttonMobile.addEventListener("click", mobMenu);
+buttonTop.addEventListener("click", topFunction);
 
 /* window.onscroll = function () {
   myFunction();
